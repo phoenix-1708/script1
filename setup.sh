6ba5e677878e0ee6f182e958bc4544784921972d
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-10-23 09:46:20 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git config --global user.name "phoenix-1708"
+git config --global user.email "harikumar1708@gmail.com"
+git config --global color.ui true
+apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+apt-get install -y openjdk-11-jdk
+apt-get install -y wget
+apt-get install -y jq
+chmod +x setup.sh build.sh upload.sh sync.sh
+git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
+mkdir -p ~/.config/rclone
+cp rclone.conf ~/.config/rclone/rclone.conf
+df -h && free -h && nproc
+mkdir -p /tmp/ccache
