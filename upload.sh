adf42ff478d87e1f1655fc3ff0c4826adc923efd
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-10-23 09:48:14 +0530

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /tmp/rom
+source build/envsetup.sh
+lunch spark_sweet-user
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 12G
+ccache -o compression=true
+ccache -z
+export TZ=Asia/Dhaka
+mka bacon -j$(nproc --all)
