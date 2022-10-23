f7199dfec60b244ff8e174f4e01efb6751604aac
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-10-23 16:54:02 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,13 +3,13 @@ cd $(pwd)/tmp
 mkdir rom
 cd rom
 echo "initialising repo"
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://gitlab.com/CalyxOS/platform_manifest -b android13 -g default,-mips,-darwin,-notdefault
 echo "Syncing source"
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 echo "cloning device"
-git clone https://github.com/phoenix-1708/android_device_xiaomi_sweet -b arrow-13.0 --depth=1 device/xiaomi/sweet
+git clone https://github.com/phoenix-1708/device_xiaomi_sweet -b calyx --depth=1 device/xiaomi/sweet
 echo "cloning vendor"
-git clone https://github.com/phoenix-1708/vendor_device_xiaomi_sweet -b arrow-13.0 --depth=1 vendor/xiaomi/sweet
+git clone https://github.com/phoenix-1708/vendor_xiaomi_sweet -b main --depth=1 vendor/xiaomi/sweet
 echo "cloning proton-clang"
 git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 prebuilts/clang/host/linux-x86/clang-proton
 echo "cloning kernel"
