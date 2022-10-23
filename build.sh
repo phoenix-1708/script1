23db0e3c03189f86837d1153b040b0e66f0191ae
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-10-23 16:55:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source build/envsetup.sh
-lunch spark_sweet-user
+lunch calyx_sweet-user
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -8,4 +8,4 @@ ccache -M 12G
 ccache -o compression=true
 ccache -z
 export TZ=Asia/Dhaka
-mka bacon -j$(nproc --all)
+m -j$(nproc --all)
