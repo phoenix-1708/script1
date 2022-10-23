4dcf5b4b0775114902d4a5528fd0871c40a6d222
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-10-23 11:57:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 git config --global user.name "phoenix-1708"
 git config --global user.email "harikumar1708@gmail.com"
 git config --global color.ui true
-apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-apt-get install -y openjdk-11-jdk
-apt-get install -y wget
-apt-get install -y jq
+sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt-get install -y openjdk-11-jdk
+sudo apt-get install -y wget
+sudo apt-get install -y jq
 chmod +x setup.sh build.sh upload.sh sync.sh
 git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
 mkdir -p ~/.config/rclone
