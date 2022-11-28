41b3e40b8f20622f8bf48548ea40a991a23e49a9
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-11-28 10:32:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 source build/envsetup.sh
-lunch calyx_sweet-user
+lunch lineage_sweet-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 12G
 ccache -o compression=true
 ccache -z
-export TZ=Asia/Dhaka
-m -j$(nproc --all)
+export BUILD_USERNAME=phoenix1708
+export BUILD_HOSTNAME=prototype
+export TZ=Asia/Jakarta
+mka bacon -j$(nproc --all)
