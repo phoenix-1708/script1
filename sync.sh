92d8ebaa7aee20dd1b1770863b2326cda2468b20
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-11-28 10:30:25 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,17 +3,28 @@ cd $(pwd)/tmp
 mkdir rom
 cd rom
 echo "initialising repo"
-repo init --depth=1 --no-repo-verify -u https://gitlab.com/CalyxOS/platform_manifest -b android13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+
 echo "Syncing source"
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 echo "cloning device"
-git clone https://github.com/phoenix-1708/device_xiaomi_sweet -b calyx --depth=1 device/xiaomi/sweet
+git clone https://github.com/phoenix-1708/device_sweet -b main --depth=1 device/xiaomi/sweet
+
 echo "cloning vendor"
-git clone https://github.com/phoenix-1708/vendor_xiaomi_sweet -b main --depth=1 vendor/xiaomi/sweet
-echo "cloning proton-clang"
-git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/phoenix-1708/vendor_sweet -b main --depth=1 vendor/xiaomi/sweet
+
+echo "cloning clang"
+git clone https://gitlab.com/ImSurajxD/clang-r450784d -b master --depth=1 prebuilts/clang/host/linux-x86/clang-r450784d
+git clone https://gitlab.com/nerdprojectorg/SDClang -b 16 --depth=1 prebuilts/clang/host/linux-x86/clang-sdclang
+
+echo "cloning dolby"
+git clone https://gitlab.com/someone5678/vendor_dolby -b thirteen --depth=1 vendor/dolby
+
 echo "cloning kernel"
-git clone https://github.com/olhotorto/kernel_xiaomi_sweet -b dev/13 --depth=1 kernel/xiaomi/sweet
+git clone https://github.com/phoenix-1708/kernel_xiaomi_sm6150 -b t13 --depth=1 kernel/xiaomi/sweet
+
 echo"cloning finished"
+
 cp kernel/xiaomi/sweet/arch/arm64/configs/sweet_defconfig kernel/xiaomi/sweet/arch/arm64/configs/sweet_user_defconfig
 echo "file copied successfully"
