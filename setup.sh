4cbc155e838aa2fc8c5f8be23aea9ad9b75454c2
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-12-23 21:51:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,14 @@ git config --global user.email "harikumar1708@gmail.com"
 git config --global color.ui true
 sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 sudo apt-get install -y openjdk-11-jdk
-sudo apt-get install -y wget
+sudo apt-get install -y wget pigz
 sudo apt-get install -y jq
 chmod +x setup.sh build.sh upload.sh sync.sh
 git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
 mkdir -p ~/.config/rclone
 cp rclone.conf ~/.config/rclone/rclone.conf
 df -h && free -h && nproc
-mkdir -p /home/harikumar/ccache
+mkdir -p /home/harikumar/tmp
+cd /home/harikumar/tmp
+wget https://oshi.at/YtQE -O ccache.tar.gz --show-progress
+time tar xvf ccache.tar.gz
